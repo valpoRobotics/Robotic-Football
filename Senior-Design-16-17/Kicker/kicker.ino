90a068c82065ff3bebce8f7bb3cf74c4aeaa98a9
blendergeek <aroggow@hotmail.com>
2016-12-06 17:04:07 -0600

    Create kicker.ino

---
+++ Senior-Design-16-17/Kicker/kicker.ino
@@ -0,0 +1,9 @@
+/*
+  Software developed for use on Charlie Brown (Kicker Gen 3)
+  Developed by Aaron Roggow(17) and John White(17)
+  
+  Change log:
+  Date                  Name                Notes
+  ======                ====                =====
+  DEC/6/16              AWR                 file creation
+*/
