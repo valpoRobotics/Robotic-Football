6c4c12ab48daa65c2c0e1bf50c3e6c928c0c4eee
blendergeek <aroggow@hotmail.com>
2017-03-30 17:41:30 -0500

    Update kicker.ino
    ADDED MECHANICAL LOCK SERVO

--- Senior-Design-16-17/Kicker/kicker.ino
+++ Senior-Design-16-17/Kicker/kicker.ino
@@ -1,13 +1,14 @@
 /*
   Software developed for use on Charlie Brown (Kicker Gen 3)
   Developed by Aaron Roggow(17) and John White(17)
-  
+
   Change log:
   Date                   Name                 Notes
   ======                 =====                =====
   DEC/06/16               AWR                 file creation, drivetrain written
   JAN/19/17               AWR                 updated debug messaging, added kids mode
   JAN/26/17               AWR                 added kicking, electronic lockout
+  MAR/30/17               AWR                 added mechanical lock servo
 */
 
 #include <PS3BT.h>
@@ -24,6 +25,7 @@
 #define RIGHT_MOTOR   10
 #define KICKER_MOTOR  8
 #define LOCKOUT_PIN   7
+#define SERVO_MOTOR   5
 
 
 #define LEFT_FLIP -1
@@ -39,6 +41,12 @@ bool kidMode = false;
 Servo leftMotor;                  //Left motor
 Servo rightMotor;                 //Right motor
 Servo kickerMotor;                //Center motor
+Servo servoMotor;
+
+#define SERVO_LOCK_POSITION 125
+#define SERVO_UNLOCK_POSITION 180
+bool servoLocked = false;
+
 bool lockout;                     // 1 if in lockout mode, 0 if ready for kick
 unsigned long timeOfLastLockout = 0;
 #define TRIANGLE_KICK_VALUE  180
@@ -70,6 +78,9 @@ void setup() {
   leftMotor.attach(LEFT_MOTOR,      1000, 2000);
   rightMotor.attach(RIGHT_MOTOR,    1000, 2000);
   kickerMotor.attach(KICKER_MOTOR,  1000, 2000);
+  servoMotor.attach(SERVO_MOTOR);//,     900, 2100);
+  servoMotor.write(SERVO_UNLOCK_POSITION);
+
   pinMode(LOCKOUT_PIN, INPUT);
   stop();
 
@@ -96,19 +107,19 @@ void loop() {
     if (newconnect == 0)
     {
       newconnect = 1;
-      #ifdef DEBUG
+#ifdef DEBUG
       Serial.println("Connection is good!");
-      #endif
+#endif
       PS3.moveSetRumble(64);
-      PS3.setRumbleOn(50, 255, 50, 255); //VIBRATE!!!    
+      PS3.setRumbleOn(50, 255, 50, 255); //VIBRATE!!!
       PS3.setLedRaw(1);
       setGreen();
     }
     if (PS3.getButtonClick(PS))
     {
-      #ifdef DEBUG
+#ifdef DEBUG
       Serial.println("Disconnect");
-      #endif
+#endif
       kidMode = false;
       PS3.disconnect();
       newconnect = 0;
@@ -116,16 +127,16 @@ void loop() {
       stop();
     }
 
-    if(PS3.getButtonPress(SELECT))
+    if (PS3.getButtonPress(SELECT))
     {
-      if(PS3.getButtonClick(START))
+      if (PS3.getButtonClick(START))
       {
-        if(!kidMode)
+        if (!kidMode)
         {
           kidMode = true;
-          #ifdef DEBUG
+#ifdef DEBUG
           Serial.print("Entering Kid Mode ");
-          #endif
+#endif
           PS3.setRumbleOff();
           PS3.setLedRaw(15);
           currentHandicap = KID_HANDICAP;
@@ -133,39 +144,39 @@ void loop() {
         else
         {
           kidMode = false;
-          #ifdef DEBUG
+#ifdef DEBUG
           Serial.print("Exiting Kid Mode ");
-          #endif
+#endif
           PS3.setRumbleOff();
           PS3.setLedRaw(1);
           currentHandicap = HANDICAP;
         }
       }
     }
-    
+
     if (PS3.getButtonPress(R2) && !kidMode)
     {
-      #ifdef DEBUG
+#ifdef DEBUG
       Serial.print("Turbo! ");
-      #endif
+#endif
       currentHandicap = TURBO;
     }
     else if (!kidMode) currentHandicap = HANDICAP;
-    else 
+    else
     {
       currentHandicap = KID_HANDICAP;
-      #ifdef DEBUG
+#ifdef DEBUG
       Serial.print("Kid Mode! ");
-      #endif
+#endif
     }
-  
+
     int yInput = map(PS3.getAnalogHat(LeftHatY), 0, 255, -90, 90); //Recieves PS3 forward/backward input
     int xInput = map(PS3.getAnalogHat(RightHatX), 0, 255, 90, -90); //Recieves PS3 horizontal input and sets it to an inverted scale of 90 to -90
     setGreen();
 
     if (abs(yInput) < DEADZONE) yInput = 0;
     if (abs(xInput) < DEADZONE) xInput = 0;
-    
+
     //Instead of following some sort of equation to slow down acceleration
     //We just increment the speed by one towards the desired speed.
     //The acceleration is then slowed because of the loop cycle time
@@ -183,93 +194,120 @@ void loop() {
     int ThrottleR = RIGHT_FLIP * ((Drive - Turn) / currentHandicap);
 
     if (ThrottleL > 90) ThrottleL = 90;
-    else if(ThrottleL < -90) ThrottleL = -90;
+    else if (ThrottleL < -90) ThrottleL = -90;
     if (ThrottleR > 90) ThrottleR = 90;
-    else if(ThrottleR < -90) ThrottleR = -90;
+    else if (ThrottleR < -90) ThrottleR = -90;
 
-    #ifdef DEBUG
+#ifdef DEBUG
     Serial.print("ThrottleL: ");
     Serial.print(ThrottleL);
     Serial.print(" ThrottleR: ");
     Serial.print(ThrottleR);
     Serial.print(" motorCorrect: ");
     Serial.print(motorCorrect);
-    
-    #endif
+
+#endif
     leftMotor.write((ThrottleL + 90 + motorCorrect)); //Sending values to the speed controllers
     rightMotor.write((ThrottleR + 90 + motorCorrect));
 
-    lockout = digitalRead(LOCKOUT_PIN); 
-    if(PS3.getButtonPress(R1) && !kidMode) 
+    if (PS3.getButtonClick(L1))
     {
+      servoMotor.write(SERVO_LOCK_POSITION);
+      servoLocked = true;
+#ifdef DEBUG
+      Serial.print(" Servo Locked ");
+#endif
+    }
+
+
+    lockout = digitalRead(LOCKOUT_PIN);
+    /*if(PS3.getButtonPress(R1) && !kidMode)
+      {
       #ifdef DEBUG
       Serial.print(" Kicking Power: Reloading");
       #endif
       kickerMotor.write(RELOAD_VALUE);
-    }
-    else if(!lockout && !kidMode)   //cleared for kick
+      }
+      else */if (!lockout && !kidMode)  //cleared for kick
     {
-      if((millis() - timeOfLastLockout) > LOCKOUT_DELAY_TIME)
+      if ((millis() - timeOfLastLockout) > LOCKOUT_DELAY_TIME)
       {
-        #ifdef DEBUG
+        if (PS3.getButtonClick(R1))
+        {
+          servoMotor.write(SERVO_UNLOCK_POSITION);
+          servoLocked = false;
+#ifdef DEBUG
+          Serial.print(" Servo Unlocked ");
+#endif
+        }
+#ifdef DEBUG
         Serial.print(" Kicking Power: ");
-        #endif
-        if(PS3.getButtonPress(TRIANGLE))    
+#endif
+        if (!servoLocked)
         {
-          #ifdef DEBUG
-          Serial.print(TRIANGLE_KICK_VALUE);
-          #endif
-          kickerMotor.write(TRIANGLE_KICK_VALUE);
+          if (PS3.getButtonPress(TRIANGLE))
+          {
+#ifdef DEBUG
+            Serial.print(TRIANGLE_KICK_VALUE);
+#endif
+            kickerMotor.write(TRIANGLE_KICK_VALUE);
+          }
+          else if (PS3.getButtonPress(CIRCLE))
+          {
+#ifdef DEBUG
+            Serial.print(CIRCLE_KICK_VALUE);
+#endif
+            kickerMotor.write(CIRCLE_KICK_VALUE);
+          }
+          else if (PS3.getButtonPress(CROSS))
+          {
+#ifdef DEBUG
+            Serial.print(CROSS_KICK_VALUE);
+#endif
+            kickerMotor.write(CROSS_KICK_VALUE);
+          }
+          else if (PS3.getButtonPress(SQUARE))
+          {
+#ifdef DEBUG
+            Serial.print(SQUARE_KICK_VALUE);
+#endif
+            kickerMotor.write(SQUARE_KICK_VALUE);
+          }
         }
-        else if(PS3.getButtonPress(CIRCLE)) 
+        else
         {
-          #ifdef DEBUG
-          Serial.print(CIRCLE_KICK_VALUE);
-          #endif
-          kickerMotor.write(CIRCLE_KICK_VALUE);
-        }
-        else if(PS3.getButtonPress(CROSS))  
-        {
-          #ifdef DEBUG
-          Serial.print(CROSS_KICK_VALUE);
-          #endif
-          kickerMotor.write(CROSS_KICK_VALUE);
-        }
-        else if(PS3.getButtonPress(SQUARE)) 
-        {
-          #ifdef DEBUG
-          Serial.print(SQUARE_KICK_VALUE);
-          #endif
-          kickerMotor.write(SQUARE_KICK_VALUE);
-        }
-        else 
-        {
-          #ifdef DEBUG
+#ifdef DEBUG
           Serial.print(0);
-          #endif
+#endif
           kickerMotor.writeMicroseconds(1500);
         }
+        /*
+        if (PS3.getButtonClick(R1))
+        {
+          servoMotor.write(SERVO_UNLOCK_POSITION);
+        }*/
       }
       else
       {
-        #ifdef DEBUG
+#ifdef DEBUG
         Serial.print("Time since last lockout: ");
         Serial.print(millis() - timeOfLastLockout);
-        #endif
+#endif
       }
-      
+
     }
     else           // do not kick!
     {
-      #ifdef DEBUG
+#ifdef DEBUG
       Serial.print(" In Lockout Mode");
-      #endif
+#endif
       kickerMotor.writeMicroseconds(1500);
+
       timeOfLastLockout = millis();
     }
     Serial.println(";");
   }
-      
+
   else
   {
     stop();
@@ -283,9 +321,9 @@ void stop()
   leftMotor.writeMicroseconds(1500);
   rightMotor.writeMicroseconds(1500);
   kickerMotor.writeMicroseconds(1500);
-  #ifdef DEBUG
+#ifdef DEBUG
   Serial.println("Stop");
-  #endif
+#endif
 }
 
 void flashLEDs()
