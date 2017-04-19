6090ef2bd4a651b0108a509d4dd40b86bb7ddca3
Bill-Sullivan <william.sullivan@valpo.edu>
2017-04-19 01:14:57 -0500

    Update Robotic-Football-All-In-One.ino

--- Robotic-Football-All-In-One/Robotic-Football-All-In-One.ino
+++ Robotic-Football-All-In-One/Robotic-Football-All-In-One.ino
@@ -452,20 +452,20 @@ void loop()
     {
    // begin to move
 	// These should be moved to the top when integrated into main branch
-	#define arcadeMode 0
-	#define tankMode   1
-	#define driveModeAddress 0
+	#define ARCADE_MODE 0
+	#define TANK_MODE   1
+	#define DRIVE_MODE_ADDRESS 0
 	// end to move
       if (PS3.getButtonClick(SELECT)) //Switch between tank drive and arcade mode. 0 is arcade 1 is tank
       {
         if (PS3.getButtonPress(L1)) {
-          if (driveState == arcadeMode) {
-            EEPROM.write(driveModeAddress, tankMode);
-            driveState = tankMode;
+          if (driveState == ARCADE_MODE) {
+            EEPROM.write(DRIVE_MODE_ADDRESS, TANK_MODE);
+            driveState = TANK_MODE;
           }
-          else if (driveState == tankMode) {
-            EEPROM.write(driveModeAddress, arcadeMode);
-            driveState = arcadeMode;
+          else if (driveState == TANK_MODE) {
+            EEPROM.write(DRIVE_MODE_ADDRESS, ARCADE_MODE);
+            driveState = ARCADE_MODE;
           }
         }
         //PS3.setLedRaw(15);                // ON ON ON ON
